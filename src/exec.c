c3c76d7966d837087a92fc613fb50ce45cbd975f
Fintan Smyth <fsmyth@c1r3s3.42london.com>
2025-01-30 15:07:05 +0000

    added check of filetype to execution

--- src/exec.c
+++ src/exec.c
@@ -6,11 +6,12 @@
 /*   By: fsmyth <fsmyth@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:39:24 by fsmyth            #+#    #+#             */
-/*   Updated: 2025/01/29 16:59:53 by fsmyth           ###   ########.fr       */
+/*   Updated: 2025/01/30 14:43:50 by fsmyth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <sys/stat.h>
 
 int	count_args(char **args)
 {
@@ -24,14 +25,17 @@ int	count_args(char **args)
 
 int	search_path(t_term *term, char *cmd, char *cmd_path)
 {
-	char	**paths;
-	int		i;
+	struct stat *statbuf;
+	char		**paths;
+	int			i;
 
 	i = 0;
+	statbuf = malloc(sizeof(*statbuf));
 	paths = ft_split(getenv_list(term->env_list, "PATH"), ':');
 	ft_bzero(cmd_path, PATH_MAX);
 	ft_strlcat(cmd_path, cmd, PATH_MAX);
-	while (access(cmd_path, X_OK))
+	stat(cmd_path, statbuf);
+	while (access(cmd_path, X_OK) || !(statbuf->st_mode & S_IFREG))
 	{
 		ft_bzero(cmd_path, PATH_MAX);
 		if (paths[i] == NULL)
@@ -39,8 +43,10 @@ int	search_path(t_term *term, char *cmd, char *cmd_path)
 		ft_strlcpy(cmd_path, paths[i++], PATH_MAX);
 		ft_strlcat(cmd_path, "/", PATH_MAX);
 		ft_strlcat(cmd_path, cmd, PATH_MAX);
+		stat(cmd_path, statbuf);
 	}
 	free_split(&paths);
+	free(statbuf);
 	if (cmd_path[0] == 0)
 		return (1);
 	return (0);
