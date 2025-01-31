8ef2685d17fc30fea5c8751ab6b6d98ad20eb763
Fintan Smyth <fsmyth@c1r5s2.42london.com>
2025-01-31 16:33:27 +0000

    tweaked echo format

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -36,7 +36,7 @@ void	echo(t_cmd *cmd)
 		}
 		if (nl == 1)
 			write(cmd->fd_out, "\n", 1);
-		// else if (i != 2)
-		// 	ft_putstr_fd("\e[7m%\e[m", cmd->fd_out);
+		else if (i != 2 && cmd->fd_out == 1)
+			ft_putstr_fd("\e[7m%\e[m\n", 1);
 	}
 }
