5e5ab5e35ea6b21027ee315dd312553259965c7d
Fintan Smyth <fsmyth@c1r4s1.42london.com>
2025-02-02 18:14:26 +0000

    Changed debug script to allow choice on debug printing

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MS_DEBUG=ON valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --suppressions=readline.supp -s ./minishell
+MS_DEBUG=$1 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --suppressions=readline.supp -s ./minishell
